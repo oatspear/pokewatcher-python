683a5fc953f198447652ed3b3a965280daeeb7ce
oatspear <oatspear@gmail.com>
2022-08-10 15:24:14 +0100

    Create constants.py

---
+++ src/pokewatcher/data/yellow/constants.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: MIT
+# Copyright © 2022 André "Oatspear" Santos
+
+###############################################################################
+# Imports
+###############################################################################
+
+from typing import Final
+
+###############################################################################
+# Constants
+###############################################################################
+
+BATTLE_TYPE_NONE: Final[int] = 0x00
+BATTLE_TYPE_WILD: Final[int] = 0x01
+BATTLE_TYPE_TRAINER: Final[int] = 0x02
+BATTLE_TYPE_LOST: Final[int] = 0xFF
