d3ad69c18ca4916dc19d7c4580523f4993e466c3
oatspear <oatspear@gmail.com>
2022-08-10 15:52:23 +0100

    Update constants.py

--- src/pokewatcher/data/yellow/constants.py
+++ src/pokewatcher/data/yellow/constants.py
@@ -15,3 +15,6 @@ BATTLE_TYPE_NONE: Final[int] = 0x00
 BATTLE_TYPE_WILD: Final[int] = 0x01
 BATTLE_TYPE_TRAINER: Final[int] = 0x02
 BATTLE_TYPE_LOST: Final[int] = 0xFF
+
+ALARM_ENABLED: Final[int] = 0x00
+ALARM_DISABLED: Final[int] = 0x01
