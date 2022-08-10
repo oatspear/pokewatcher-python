7730822ce3a56ba489df453910c3482baca1f219
oatspear <oatspear@gmail.com>
2022-08-10 10:51:27 +0100

    Update errors.py

--- src/pokewatcher/errors.py
+++ src/pokewatcher/errors.py
@@ -2,9 +2,21 @@
 # Copyright © 2022 André "Oatspear" Santos
 
 ###############################################################################
+# Imports
+###############################################################################
+
+from typing import Any
+
+###############################################################################
 # Base Class
 ###############################################################################
 
 
 class PokeWatcherError(Exception):
     pass
+
+
+class StateMachineError(PokeWatcherError):
+    @classmethod
+    def no_transition(cls, state: str, label: str, value: Any) -> 'StateMachineError':
+        return cls(f'No transition from state {state} via {label} ({value})')
