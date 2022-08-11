d8b886525dce2a5f29137a04d2dde810d4b096be
oatspear <oatspear@gmail.com>
2022-08-11 11:30:32 +0100

    Update gamehook.py

--- src/pokewatcher/data/yellow/gamehook.py
+++ src/pokewatcher/data/yellow/gamehook.py
@@ -5,15 +5,15 @@
 # Imports
 ###############################################################################
 
-from typing import Any, Final, Mapping
+from typing import Final, Mapping
 
 import logging
 
-from attrs import define
-
 import pokewatcher.data.constants as const
-from pokewatcher.data.gamehook import BaseDataHandler
-import pokewatcher.events as events
+import pokewatcher.data.yellow.constants as yellow
+from pokewatcher.data.gamehook import DataHandler
+from pokewatcher.data.structs import GameData
+from pokewatcher.logic.fsm import StateMachine
 
 ###############################################################################
 # Constants
@@ -73,14 +73,11 @@ P_BADGE6 = 'player.badges.marshBadge'
 P_BADGE7 = 'player.badges.volcanoBadge'
 P_BADGE8 = 'player.badges.earthBadge'
 
-SFX_SAVE_FILE = 0xB6
-
-TRAINER_CLASS_NONE = 'NOBODY'
-
-LABELS = {
-    P_PLAYER_ID: 'wPlayerID',
-    P_BATTLE_TYPE: 'wIsInBattle',
-    P_BATTLE_ALARM: 'wLowHealthAlarmDisabled',
+LABELS: Final[Mapping[str, str]] = {
+    P_PLAYER_ID: yellow.WRAM_PLAYER_ID,
+    P_BATTLE_TYPE: yellow.WRAM_BATTLE_TYPE,
+    P_BATTLE_ALARM: yellow.WRAM_LOW_HEALTH_ALARM,
+    P_AUDIO_CH5: yellow.WRAM_AUDIO_CHANNEL5,
 }
 
 ###############################################################################
@@ -88,63 +85,55 @@ LABELS = {
 ###############################################################################
 
 
-@define
-class DataHandler(BaseDataHandler):
-    def __attrs_post_init__(self):
-        self.labels.update(LABELS)
+def load_data_handler(data: GameData, fsm: StateMachine) -> DataHandler:
+    handler = DataHandler()
 
-        # player data
-        self.store(P_PLAYER_ID, const.VAR_PLAYER_ID)
-        self.store(P_GAME_TIME_HOURS, const.VAR_GAME_TIME_HOURS)
-        self.store(P_GAME_TIME_MINUTES, const.VAR_GAME_TIME_MINUTES)
-        self.store(P_GAME_TIME_SECONDS, const.VAR_GAME_TIME_SECONDS)
-        self.store(P_BADGE1, const.VAR_PLAYER_BADGE1)
-        self.store(P_BADGE2, const.VAR_PLAYER_BADGE2)
-        self.store(P_BADGE3, const.VAR_PLAYER_BADGE3)
-        self.store(P_BADGE4, const.VAR_PLAYER_BADGE4)
-        self.store(P_BADGE5, const.VAR_PLAYER_BADGE5)
-        self.store(P_BADGE6, const.VAR_PLAYER_BADGE6)
-        self.store(P_BADGE7, const.VAR_PLAYER_BADGE7)
-        self.store(P_BADGE8, const.VAR_PLAYER_BADGE8)
+    # state machine logic
+    handler.labels.update(LABELS)
 
-        # world data
-        self.store(P_MAP, const.VAR_MAP)
+    # player data
+    handler.store(P_PLAYER_ID, const.VAR_PLAYER_ID)
+    handler.store(P_GAME_TIME_HOURS, const.VAR_GAME_TIME_HOURS)
+    handler.store(P_GAME_TIME_MINUTES, const.VAR_GAME_TIME_MINUTES)
+    handler.store(P_GAME_TIME_SECONDS, const.VAR_GAME_TIME_SECONDS)
+    handler.store(P_BADGE1, const.VAR_PLAYER_BADGE1)
+    handler.store(P_BADGE2, const.VAR_PLAYER_BADGE2)
+    handler.store(P_BADGE3, const.VAR_PLAYER_BADGE3)
+    handler.store(P_BADGE4, const.VAR_PLAYER_BADGE4)
+    handler.store(P_BADGE5, const.VAR_PLAYER_BADGE5)
+    handler.store(P_BADGE6, const.VAR_PLAYER_BADGE6)
+    handler.store(P_BADGE7, const.VAR_PLAYER_BADGE7)
+    handler.store(P_BADGE8, const.VAR_PLAYER_BADGE8)
 
-        # party lead data
-        self.store(P_SPECIES, const.VAR_PARTY_MON1_SPECIES)
-        self.store(P_LEVEL, const.VAR_PARTY_MON1_LEVEL)
-        self.store(P_MON_ATK, const.VAR_PARTY_MON1_ATTACK)
-        self.store(P_MON_DEF, const.VAR_PARTY_MON1_DEFENSE)
-        self.store(P_MON_SPD, const.VAR_PARTY_MON1_SPEED)
-        self.store(P_MON_SPC, const.VAR_PARTY_MON1_SP_ATTACK)
-        self.store(P_MON_SPC, const.VAR_PARTY_MON1_SP_DEFENSE)
+    # world data
+    handler.store(P_MAP, const.VAR_MAP)
 
-        # battle data
-        # self.store_int(P_GYM_LEADER)
-        self.store(P_TRAINER_CLASS, const.VAR_BATTLE_TRAINER_CLASS)
-        self.store(P_TRAINER_NUMBER, const.VAR_BATTLE_TRAINER_ID)
+    # party lead data
+    handler.store(P_SPECIES, const.VAR_PARTY_MON1_SPECIES)
+    handler.store(P_LEVEL, const.VAR_PARTY_MON1_LEVEL)
+    handler.store(P_MON_ATK, const.VAR_PARTY_MON1_ATTACK)
+    handler.store(P_MON_DEF, const.VAR_PARTY_MON1_DEFENSE)
+    handler.store(P_MON_SPD, const.VAR_PARTY_MON1_SPEED)
+    handler.store(P_MON_SPC, const.VAR_PARTY_MON1_SP_ATTACK)
+    handler.store(P_MON_SPC, const.VAR_PARTY_MON1_SP_DEFENSE)
 
-        # battle lead data
-        self.store(P_BATTLE_ATK, const.VAR_BATTLE_PLAYER_ATTACK)
-        self.store(P_BATTLE_DEF, const.VAR_BATTLE_PLAYER_DEFENSE)
-        self.store(P_BATTLE_SPD, const.VAR_BATTLE_PLAYER_SPEED)
-        self.store(P_BATTLE_SPC, const.VAR_BATTLE_PLAYER_SP_ATTACK)
-        self.store(P_BATTLE_SPC, const.VAR_BATTLE_PLAYER_SP_DEFENSE)
-        self.store(P_STAGE_ATK, const.VAR_BATTLE_PLAYER_STAGE_ATTACK)
-        self.store(P_STAGE_DEF, const.VAR_BATTLE_PLAYER_STAGE_DEFENSE)
-        self.store(P_STAGE_SPD, const.VAR_BATTLE_PLAYER_STAGE_SPEED)
-        self.store(P_STAGE_SPC, const.VAR_BATTLE_PLAYER_STAGE_SP_ATTACK)
-        self.store(P_STAGE_SPC, const.VAR_BATTLE_PLAYER_STAGE_SP_DEFENSE)
-        self.store(P_STAGE_ACC, const.VAR_BATTLE_PLAYER_STAGE_ACCURACY)
-        self.store(P_STAGE_EVA, const.VAR_BATTLE_PLAYER_STAGE_EVASION)
+    # battle data
+    # handler.store(P_GYM_LEADER, ?)
+    handler.store(P_TRAINER_CLASS, const.VAR_BATTLE_TRAINER_CLASS)
+    handler.store(P_TRAINER_NUMBER, const.VAR_BATTLE_TRAINER_ID)
 
-        # other game aspects
-        # self.handlers[P_AUDIO_SOUND] = self.on_audio_changed
-        self.handlers[P_AUDIO_CH5] = self.on_audio_changed
-        # self.handlers[P_AUDIO_CH6] = self.on_audio_changed
+    # battle lead data
+    handler.store(P_BATTLE_ATK, const.VAR_BATTLE_PLAYER_ATTACK)
+    handler.store(P_BATTLE_DEF, const.VAR_BATTLE_PLAYER_DEFENSE)
+    handler.store(P_BATTLE_SPD, const.VAR_BATTLE_PLAYER_SPEED)
+    handler.store(P_BATTLE_SPC, const.VAR_BATTLE_PLAYER_SP_ATTACK)
+    handler.store(P_BATTLE_SPC, const.VAR_BATTLE_PLAYER_SP_DEFENSE)
+    handler.store(P_STAGE_ATK, const.VAR_BATTLE_PLAYER_STAGE_ATTACK)
+    handler.store(P_STAGE_DEF, const.VAR_BATTLE_PLAYER_STAGE_DEFENSE)
+    handler.store(P_STAGE_SPD, const.VAR_BATTLE_PLAYER_STAGE_SPEED)
+    handler.store(P_STAGE_SPC, const.VAR_BATTLE_PLAYER_STAGE_SP_ATTACK)
+    handler.store(P_STAGE_SPC, const.VAR_BATTLE_PLAYER_STAGE_SP_DEFENSE)
+    handler.store(P_STAGE_ACC, const.VAR_BATTLE_PLAYER_STAGE_ACCURACY)
+    handler.store(P_STAGE_EVA, const.VAR_BATTLE_PLAYER_STAGE_EVASION)
 
-    def on_audio_changed(self, prev: Any, value: Any, mapper: Mapping[str, Any]):
-        if self.state != STATE_OUT_OF_BATTLE:
-            return
-        if value == SFX_SAVE_FILE:
-            events.on_save_game.emit()
+    return handler
