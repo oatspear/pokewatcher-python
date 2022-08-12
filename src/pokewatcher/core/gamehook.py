d7922c0031ce0fbc9d7152eb513c1d3d667ef882
oatspear <oatspear@gmail.com>
2022-08-12 10:56:35 +0100

    Update gamehook.py

--- src/pokewatcher/core/gamehook.py
+++ src/pokewatcher/core/gamehook.py
@@ -15,20 +15,13 @@ from attrs import define, field
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 
 from pokewatcher.errors import PokeWatcherError
-from pokewatcher.core.util import identity, noop, SleepLoop
+from pokewatcher.core.util import noop, SleepLoop
 
 ###############################################################################
 # Constants
 ###############################################################################
 
 logger: Final = logging.getLogger(__name__)
-
-TRANSFORMS: Final[Mapping[str, Callable]] = {
-    'bool': bool,
-    'int': int,
-    'float': float,
-    'string': str,
-}
 
 ###############################################################################
 # Interface
@@ -54,8 +47,6 @@ class GameHookBridge:
     on_load: Callable = noop
     meta: Dict[str, Any] = field(init=False, factory=dict)
     mapper: Dict[str, Any] = field(init=False, factory=dict)
-    transforms: Dict[str, Callable] = field(init=False, factory=dict)
-    byte_properties: Set[str] = field(init=False, factory=set)
     url_signalr: str = field(init=False, default='http://localhost:8085/updates')
     url_requests: str = field(init=False, default='http://localhost:8085/mapper')
     hub: Optional[HubConnectionBuilder] = field(init=False, default=None, repr=False)
@@ -71,14 +62,6 @@ class GameHookBridge:
         self.url_signalr = urls['signalr']
         self.url_requests = urls['requests']
         self.request_mapper()
-        # FIXME this cannot be here, must be version-specific
-        for prop, conf in settings['properties'].items():
-            data_type = conf.get('type', '')
-            key = conf.get('key', '')
-            self.transforms[prop] = get_transform(data_type, key)
-            use_bytes = bool(conf.get('bytes', False))
-            if use_bytes is True:
-                self.byte_properties.add(prop)
 
     def start(self):
         self.connect()
@@ -141,22 +124,10 @@ class GameHookBridge:
     def _on_property_changed(self, args):
         prop, _address, value, byte_value, _frozen, changed_fields = args
         if 'value' in changed_fields:
-            if prop in self.byte_properties:
-                value = byte_value
-            f = self.transforms.get(prop, identity)
-            value = f(value)
-            prev = self.mapper.get(prop)
+            self.on_change(prop, value, byte_value)
             self.mapper[prop] = value
-            self.on_change(prop, prev, value, self.mapper)
 
 
 def new():
     instance = GameHookBridge()
     return instance
-
-
-def get_transform(data_type: str, key: str):
-    transform = TRANSFORMS.get(data_type, identity)
-    if key:
-        return lambda d: transform(d[key])
-    return transform
