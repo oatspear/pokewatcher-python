7c462a9e8316be8c102bd4590bf857ab7ee900c8
oatspear <oatspear@gmail.com>
2022-08-16 10:57:07 +0100

    Update save_backup.py

--- src/pokewatcher/components/save_backup.py
+++ src/pokewatcher/components/save_backup.py
@@ -87,16 +87,7 @@ class SaveFileBackupComponent:
             logger.warning('unable to get save file path')
             return
 
-        data = {
-            'rom': self.game.rom or 'NULL',
-            'version': self.game.version or 'NULL',
-            'realtime': '0:00:00.0',
-            'player': self.game.data.player,
-            'time': self.game.data.time,
-            'location': self.game.data.location or 'NULL',
-            'battle': self.game.data.battle,
-        }
-        self.game.data.serialize()
+        data = self.game.data_dict()
 
         data['location'] = data['location'].replace(' ', '').replace('-', '')
 
