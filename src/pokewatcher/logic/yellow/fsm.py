d1b60c1ab4b568ef71a2cb2d4cae845b8cf6f997
oatspear <oatspear@gmail.com>
2022-08-18 16:19:53 +0100

    Update fsm.py

--- src/pokewatcher/logic/yellow/fsm.py
+++ src/pokewatcher/logic/yellow/fsm.py
@@ -9,7 +9,7 @@ from typing import Any, Final
 
 import logging
 
-from attrs import define
+from attrs import define, field
 
 from pokewatcher.data.structs import GameData
 from pokewatcher.data.yellow.constants import (
@@ -18,6 +18,11 @@ from pokewatcher.data.yellow.constants import (
     BATTLE_TYPE_TRAINER,
     BATTLE_TYPE_WILD,
     DEFAULT_PLAYER_NAME,
+    JOY_MASK_ALL,
+    JOY_MASK_NONE,
+    MENU_ITEM_CONTINUE,
+    MENU_ITEM_NEW_GAME,
+    SFX_PRESS_A_B,
     SFX_SAVE_FILE,
 )
 import pokewatcher.events as events
@@ -41,50 +46,151 @@ class YellowState(GameState):
     wChannelSoundIDs_5 = transition  # noqa: N815
     wLowHealthAlarmDisabled = transition  # noqa: N815
     wCurMap = transition  # noqa: N815
+    wJoyIgnore = transition  # noqa: N815
+    wXCoord = transition  # noqa: N815
+    wYCoord = transition  # noqa: N815
+    wCurrentMenuItem = transition  # noqa: N815
+    wd732_0 = transition  # noqa: N815
+    wPlayTimeHours = transition  # noqa: N815
+    wPlayTimeMinutes = transition  # noqa: N815
+    wPlayTimeSeconds = transition  # noqa: N815
+    wPlayTimeFrames = transition  # noqa: N815
 
 
 @define
 class Initial(YellowState):
     def wPlayerName(self, prev: str, value: str, _data: GameData) -> GameState:  # noqa: N815
-        logger.debug(f'player name changed from {prev!r} to {value!r}')
-        if value != DEFAULT_PLAYER_NAME:
-            logger.info('found saved game')
+        logger.debug(f'player name changed: {prev!r} -> {value!r}')
+        if value == DEFAULT_PLAYER_NAME:
+            # title screen
             return MainMenu()
-        return self
-
-    def wPlayerID(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
-        logger.debug(f'player ID changed from {prev} to {value}')
-        if value != 0 and prev == 0:
-            logger.info('starting a new game')
-            events.on_new_game.emit()
-            return InOverworld()
         return self
 
 
 @define
 class MainMenu(YellowState):
     def wPlayerID(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
-        logger.debug(f'player ID changed from {prev} to {value}')
-        if value != 0:
-            if prev <= 0:
-                logger.info('continue previous game')
-                events.on_continue.emit()
-                return InOverworld()
-            else:
-                logger.info('starting a new game')
-                events.on_new_game.emit()
-                return InOverworld()
+        logger.debug(f'player ID changed: {prev} -> {value}')
+        if value == 0:
+            return _reset_game()
+        if prev <= 0:
+            return _press_new_game()
+        return self
+
+    def wPlayerName(self, prev: str, value: str, _data: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'player name changed: {prev!r} -> {value!r}')
+        # this should update before wPlayerID
+        if value == DEFAULT_PLAYER_NAME:
+            return self
+        return self._check_saved_game(value.strip())
+
+    # def wd732_0(self, _p: Any, value: bool, _data: GameData) -> GameState:  # noqa: N815
+    #     logger.debug(f'count play time changed: {value}')
+    #     return self._check_saved_game(value)
+
+    # def wPlayTimeHours(self, _p: Any, value: int, _data: GameData) -> GameState:  # noqa: N815
+    #     logger.debug(f'play time hours changed: {value}')
+    #     return self._check_saved_game(value)
+
+    # def wPlayTimeMinutes(self, _p: Any, value: int, _data: GameData) -> GameState:  # noqa: N815
+    #     logger.debug(f'play time minutes changed: {value}')
+    #     return self._check_saved_game(value)
+
+    # def wPlayTimeSeconds(self, _p: Any, value: int, _data: GameData) -> GameState:  # noqa: N815
+    #     logger.debug(f'play time seconds changed: {value}')
+    #     return self._check_saved_game(value)
+
+    # def wPlayTimeFrames(self, _p: Any, value: int, _data: GameData) -> GameState:  # noqa: N815
+    #     logger.debug(f'play time frames changed: {value}')
+    #     return self._check_saved_game(value)
+
+    def _check_saved_game(self, value: Any) -> GameState:
+        if value:
+            logger.info('found saved game')
+            return MainMenuContinue()
+        return self
+
+
+@define
+class MainMenuContinue(YellowState):
+    _menu_item: int = field(init=False, default=MENU_ITEM_CONTINUE, eq=False, repr=False)
+    _map_changed: bool = field(init=False, default=False, eq=False, repr=False)
+    _x_changed: bool = field(init=False, default=False, eq=False, repr=False)
+    _y_changed: bool = field(init=False, default=False, eq=False, repr=False)
+
+    def wPlayerID(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'player ID changed: {prev} -> {value}')
+        if value == 0:
+            return _reset_game()
+        if prev > 0:
+            return _press_new_game()
+        return self
+
+    def wPlayerName(self, prev: str, value: str, _data: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'player name changed: {prev!r} -> {value!r}')
+        if value == DEFAULT_PLAYER_NAME and not prev.strip():
+            return _press_new_game()
+        return self
+
+    def wCurrentMenuItem(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'current menu item changed: {prev} -> {value}')
+        self._menu_item = value
+        return self
+
+    def wJoyIgnore(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'joypad ignore mask changed: {prev} -> {value}')
+        if value == JOY_MASK_ALL and prev == JOY_MASK_NONE:
+            return _press_continue()
+        return self
+
+    def wCurMap(self, prev: Any, value: str, _d: GameData) -> GameState:  # noqa: N815
+        logger.debug(f'map changed: {prev!r} -> {value!r}')
+        if not self._map_changed:
+            # first loaded map
+            self._map_changed = True
+            return self
+        if self._menu_item == MENU_ITEM_CONTINUE:
+            # already late
+            return _press_continue()
+        elif self._menu_item == MENU_ITEM_NEW_GAME:
+            return _press_new_game()
+        return self
+
+    def wXCoord(self, prev: int, value: int, data: GameData) -> GameState:  # noqa: N815
+        # safety net in case we miss an update to wJoyIgnore
+        logger.debug(f'player x coordinate changed: {prev} -> {value}')
+        if not self._x_changed:
+            # first loaded coordinates
+            self._x_changed = True
+            return self
+        if self._menu_item == MENU_ITEM_CONTINUE:
+            # already late
+            return _press_continue()
+        elif self._menu_item == MENU_ITEM_NEW_GAME:
+            return _press_new_game()
+        return self
+
+    def wYCoord(self, prev: int, value: int, data: GameData) -> GameState:  # noqa: N815
+        # safety net in case we miss an update to wJoyIgnore
+        logger.debug(f'player y coordinate changed: {prev} -> {value}')
+        if not self._y_changed:
+            # first loaded coordinates
+            self._y_changed = True
+            return self
+        if self._menu_item == MENU_ITEM_CONTINUE:
+            # already late
+            return _press_continue()
+        elif self._menu_item == MENU_ITEM_NEW_GAME:
+            return _press_new_game()
         return self
 
 
 @define
 class InGame(YellowState):
     def wPlayerID(self, prev: int, value: int, _data: GameData) -> GameState:  # noqa: N815
-        logger.debug(f'player ID changed from {prev} to {value}')
+        logger.debug(f'player ID changed: {prev} -> {value}')
         if value == 0:
-            logger.info('game reset')
-            events.on_reset.emit()
-            return MainMenu()
+            return _reset_game()
         return self
 
 
@@ -169,3 +275,26 @@ class VictorySequence(InGame):
         if v:
             self.inconsistent('wLowHealthAlarmDisabled', v)
         return self
+
+
+###############################################################################
+# Helper Functions
+###############################################################################
+
+
+def _press_new_game() -> GameState:
+    logger.info('starting a new game')
+    events.on_new_game.emit()
+    return InOverworld()
+
+
+def _press_continue() -> GameState:
+    logger.info('continue previous game')
+    events.on_continue.emit()
+    return InOverworld()
+
+
+def _reset_game() -> GameState:
+    logger.info('game reset')
+    events.on_reset.emit()
+    return Initial()
