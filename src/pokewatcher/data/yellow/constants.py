37722fbb122751a0b9d2b9ce9268b442a6869d90
oatspear <oatspear@gmail.com>
2022-08-18 11:16:22 +0100

    Update constants.py

--- src/pokewatcher/data/yellow/constants.py
+++ src/pokewatcher/data/yellow/constants.py
@@ -20,12 +20,26 @@ SFX_SAVE_FILE: Final[int] = 0xB6
 
 DEFAULT_PLAYER_NAME: Final[str] = 'NINTEN'
 
+JOY_MASK_A_BUTTON: Final[int] = 0x01
+JOY_MASK_B_BUTTON: Final[int] = 0x02
+JOY_MASK_SELECT: Final[int] = 0x04
+JOY_MASK_START: Final[int] = 0x08
+JOY_MASK_D_RIGHT: Final[int] = 0x10
+JOY_MASK_D_LEFT: Final[int] = 0x20
+JOY_MASK_D_UP: Final[int] = 0x40
+JOY_MASK_D_DOWN: Final[int] = 0x80
+JOY_MASK_ALL: Final[int] = 0xFF
+JOY_MASK_NONE: Final[int] = 0x00
+
 WRAM_PLAYER_ID: Final[str] = 'wPlayerID'
 WRAM_PLAYER_NAME: Final[str] = 'wPlayerName'
 WRAM_BATTLE_TYPE: Final[str] = 'wIsInBattle'
 WRAM_LOW_HEALTH_ALARM: Final[str] = 'wLowHealthAlarmDisabled'
 WRAM_AUDIO_CHANNEL5: Final[str] = 'wChannelSoundIDs_5'
 WRAM_CUR_MAP: Final[str] = 'wCurMap'
+WRAM_X_COORD: Final[str] = 'wXCoord'
+WRAM_Y_COORD: Final[str] = 'wYCoord'
 WRAM_COUNT_PLAY_TIME: Final[str] = 'wd732_0'
+WRAM_JOY_IGNORE: Final[str] = 'wJoyIgnore'
 
 ALL_WRAM: Final[Tuple] = tuple(v for k, v in globals().items() if k.startswith('WRAM_'))
