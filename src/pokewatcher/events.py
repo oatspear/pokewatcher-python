a324f53be8233cd4f520928acf4337756a847af3
oatspear <oatspear@gmail.com>
2022-08-19 08:14:30 +0100

    Update events.py

--- src/pokewatcher/events.py
+++ src/pokewatcher/events.py
@@ -92,4 +92,5 @@ on_map_changed: Final[Event] = Event(name='on_map_changed')
 
 on_battle_started: Final[Event] = Event(name='on_battle_started')
 on_battle_ended: Final[Event] = Event(name='on_battle_ended')
+on_champion_victory: Final[Event] = Event(name='on_champion_victory')
 on_blackout: Final[Event] = Event(name='on_blackout')
