06d99abc40531471a49f710f8925d767c4528561
oatspear <oatspear@gmail.com>
2023-04-26 14:32:09 +0100

    Update gamehook.py

--- src/pokewatcher/data/firered/gamehook.py
+++ src/pokewatcher/data/firered/gamehook.py
@@ -72,6 +72,8 @@ P_BADGE8: Final[str] = 'player.badges.badge8'
 P_POINTERS_CALLBACK1: Final[str] = 'pointers.callback1'
 P_POINTERS_CALLBACK2: Final[str] = 'pointers.callback2'
 
+P_MAP_NAME: Final[str] = 'overworld.mapName'
+
 PROPERTIES: Final[Mapping[str, Mapping[str, Any]]] = {
     P_PLAYER_NAME: {
         'type': 'string',
@@ -292,6 +294,14 @@ PROPERTIES: Final[Mapping[str, Mapping[str, Any]]] = {
         'default': 0,
         'little_endian': True,
         'label': firered.WRAM_CALLBACK2,
+    },
+    P_MAP_NAME: {
+        'type': 'string',
+        'store': game_data.VAR_MAP,
+        'processors': [
+            ['prefix', 'Kanto/'],
+        ],
+        'label': firered.WRAM_CUR_MAP,
     },
 }
 
